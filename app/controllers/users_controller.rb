bf469058287611bca1b5bf15f79db2de1e82748d
chihaso <cycyccq@gmail.com>
2021-09-20 21:29:30 +0900

    ログインしていない状態でユーザー一等は見れないようにする
    - ログインしていない場合はログインページにリダイレクトする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  before_action :redirect_not_logged_in_user_to_home
+
   def index
     @users = User.all
+  end
+
+  private
+
+  def redirect_not_logged_in_user_to_home
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
